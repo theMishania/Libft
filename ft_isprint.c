c1f6749f8cc189091a24868839b20b631a97c91d
Collio quaynis Cummings <cocummin@e3r1p4.21-school.ru>
2018-11-24 18:52:38 +0300

    no big changes

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,11 +6,11 @@
 /*   By: cocummin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 19:20:39 by cocummin          #+#    #+#             */
-/*   Updated: 2018/11/24 18:26:54 by cocummin         ###   ########.fr       */
+/*   Updated: 2018/11/24 18:51:39 by cocummin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "libft.h"
+#include "libft.h"
 
 int		ft_isprint(int c)
 {
